f87a6894b94a2f6b23da64004d7d1cfc41c2bd57
ユウマオノデラ <yuuma.30333@icloud.com>
2022-04-27 14:05:57 +0900

    first-commit

--- 20040330_yuuma/js/main.js
+++ 20040330_yuuma/js/main.js
@@ -1,14 +1,1 @@
-const openBtn = document.getElementById('openBtn');
-const closeBtn = document.getElementById('closeBtn');
-const modal = document.getElementById('modal');
-openBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-})
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-})
-window.addEventListener('click', (e) => {
-  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
-    modal.style.display = 'none';
-  }
-});
+
